9d88109f2e38664fb587120e90d3857e040880e1
farizl <farizlengha040@gmail.com>
2024-11-07 21:16:04 +0700

    Updated: finish 3des and mac function algorithm

--- PassportReader/main.swift
+++ PassportReader/main.swift
@@ -38,21 +38,26 @@ print(mrzHashPrefix)
 
 // Step 2.1 : Calculate encryption key by concat MrzHash with c1 and do SHA1 HASH again
 let c1 = "00000001"
-let ENCk = mrzHashPrefix + c1
-let ENCkData = ENCk.data(using: .utf8)
-let ENCkHash = sha1HashData(data: ENCkData!)
-let ENCkHashPrefix = ENCkHash.prefix(32)
-print("ENC Key : " + ENCkHashPrefix)
+let Kenc = mrzHashPrefix + c1
+let KencData = Kenc.data(using: .utf8)
+let KencHash = sha1HashData(data: KencData!)
+let KencHashPrefix = KencHash.prefix(32)
+print("ENC Key : " + KencHashPrefix)
 
 // Step 2.2 : Calculate MAC key by concat MrzHash with c2 and do SHA1 HASH again
 let c2 = "00000002"
-let MACk = mrzHashPrefix + c2
-let MACkData = MACk.data(using: .utf8)
-let MACkHash = sha1HashData(data: MACkData!)
-let MACkHashPrefix = MACkHash.prefix(32)
-print("MAC key : " + MACkHashPrefix)
+let Kmac = mrzHashPrefix + c2
+let KmacData = Kmac.data(using: .utf8)
+let KmacHash = sha1HashData(data: KmacData!)
+let KmacHashPrefix = KmacHash.prefix(32)
+print("MAC key : " + KmacHashPrefix)
 
 // adjust parity bit
+let ENCkAdj = AdjustParity(key: String(KencHashPrefix))
+print("ENC Adjust Key : " + ENCkAdj)
+
+let MACkadj = AdjustParity(key: String(KmacHashPrefix))
+print("MAC Adjust Key : " + MACkadj)
 
 //MARK: Step 03
 // Step 3.1 : Send APDU Command to passport for select DF in passport
@@ -69,51 +74,127 @@ for _ in 0..<16 {
 for _ in 0..<32 {
     kIFD = kIFD + hexStr[Int.random(in: 0..<16)]
 }
-print(rndIFD)
-print(kIFD)
+
 
 // Step 3.4 : Concat rndIFD / rndIC / kIFD respectively
-let s = rndIFD + /* rndIC + */ kIFD
-print(s)
+let S = rndIFD + /* rndIC + */ kIFD
+print(S)
 
-print(TripleDesEnc(input: "781723860C06C2264608F919887022120B795240CB7049B01C19B33E32804F0B", key: "AB94FDECF2674FDFB9B391F85D7F76F2"))
-//B9B391F85D7F76F2
 
+// Step 3.5 : Encrypt s on 3DES CBC with Kenc
+let Eifd = TripleDesEncCBC(input: S, key: String(Kenc))
+
+// Step 3.6 : Calculate Mac with Kmac
+let Mifd = MessageAuthenticationCodeMethodTwo(input: Eifd,key: String(Kmac))
+
+// Step 4 : Construct command and send to reader
+// APDU Command : 0082000028 + cmd_data + 28
+
+let cmdData = Eifd + Mifd
+
+// Step 4.1 : Decrypt 
+print(TripleDesDecCBC(input: "46B9342A41396CD7386BF5803104D7CEDC122B9132139BAF2EEDC94EE178534F", key: "AB94FDECF2674FDFB9B391F85D7F76F2"))
+
+
+
+/*
+######################### Utility is below ###############################
+ */
+// MARK: 3DES Encryption
 // 3DES Encryption
-func TripleDesEnc(input:String,key:String) -> String {
+func TripleDesEncCBC(input:String,key:String) -> String {
     // Divide Input to 64 bit Block
     let strArr:[String] = DivideInputToEach64BitBlock(HexInput: input)
     // Divided Key
     let index = key.index(key.startIndex,offsetBy: 16)
     let firstKey:String = String(key[..<index])
     let secondKey:String = String(key[index...])
-    
+    // Set count for loop array
+    let count = 0...(strArr.count-1)
+    var result:[String] = []
     // 1st Round
-    let fBlockfRound = DESEnc(Input: strArr[0], Key: firstKey)
-    // 2nd Round
+    for i in count {
+        if i == 0 {
+            result.append(DESEncECB(Input: DESDecECB(Input: DESEncCBC(Input: strArr[i], key: firstKey), key: secondKey), key: firstKey))
+        }else{
+            result.append(DESEncECB(Input: DESDecECB(Input: DESEncCBC(Input: strArr[i], key: firstKey,Iv: result[i-1]), key: secondKey), key: firstKey))
+        }
+    }
+    return result.joined().uppercased()
+}
 
-    // 3rd Round
-    
-    return DESEnc(Input: input, Key: firstKey)
+// 3DES Decryption
+func TripleDesDecCBC(input:String,key:String) -> String {
+    // Divide Input to 64 bit Block
+    let strArr:[String] = DivideInputToEach64BitBlock(HexInput: input)
+    // Divided Key
+    let index = key.index(key.startIndex,offsetBy: 16)
+    let firstKey:String = String(key[..<index])
+    let secondKey:String = String(key[index...])
+    // Set count for loop array
+    let count = 0...(strArr.count-1)
+    var result:[String] = []
+    // 1st Round
+    for i in count {
+        if i == 0 {
+            result.append(DESDecCBC(Input: DESEncECB(Input: DESDecECB(Input: strArr[i], key: firstKey), key: secondKey), key: firstKey))
+        }else{
+            result.append(DESDecCBC(Input: DESEncECB(Input: DESDecECB(Input: strArr[i], key: firstKey), key: secondKey), key: firstKey,Iv: strArr[i-1]))
+        }
+    }
+    return result.joined().uppercased()
+}
+
+// MARK: MAC Algorithm
+func MessageAuthenticationCodeMethodOne(input:String,key:String)->String{
+    // Divided key
+    let index = key.index(key.startIndex,offsetBy: 16)
+    let firstKey:String = String(key[..<index])
+    let secondKey:String = String(key[index...])
+    //Perform Des CBC mode for full length
+    let first = DESEncCBC(Input: input, key: firstKey)
+    // Divide Input to 64 bit Block
+    let strArr:[String] = DivideInputToEach64BitBlock(HexInput: first)
+    //Perform full on remain
+    let result = DESEncCBC(Input: DESDecCBC(Input: strArr[strArr.count-1], key: secondKey), key: firstKey)
+    return result.uppercased()
+}
+
+func MessageAuthenticationCodeMethodTwo(input:String,key:String)->String{
+    // Bit Padding
+    let input2 = input + "8000000000000000"
+    print("input2 : " + input2)
+    // Divided key
+    let index = key.index(key.startIndex,offsetBy: 16)
+    let firstKey:String = String(key[..<index])
+    let secondKey:String = String(key[index...])
+    //Perform Des CBC mode for full length
+    let first = DESEncCBC(Input: input2, key: firstKey)
+    // Divide Input to 64 bit Block
+    let strArr:[String] = DivideInputToEach64BitBlock(HexInput: first)
+    //Perform full on remain
+    let result = DESEncCBC(Input: DESDecCBC(Input: strArr[strArr.count-1], key: secondKey), key: firstKey)
+    return result.uppercased()
 }
 
 func DivideInputToEach64BitBlock(HexInput:String)->[String]{
     return HexInput.split(by: 16)
 }
 
-func DESEnc(Input:String,Key:String,Iv:String = "0000000000000000")->String{
-    //MARK: Round 1 - Encryption
+func DivideInputToEach8BitBlock(HexInput:String)->[String]{
+    return HexInput.split(by: 8)
+}
+
+func DESEncCBC(Input:String,key:String,Iv:String = "0000000000000000")->String{
     let Option = UInt32(kCCEncrypt)
     let Algorithm = UInt32(kCCAlgorithmDES)
-    //let Key = firstKey.hexadecimal! as NSData
-    let Key = Key.hexadecimal! as NSData
+    let Key = key.hexadecimal! as NSData
     let KeyLength = size_t(kCCKeySizeDES)
     let Data = Input.hexadecimal! as NSData
     let iv = Iv.hexadecimal! as NSData
     let cryptData1 = NSMutableData(length: Int(Data.length))!
     var numBytesEncrypted :size_t = 0
-    let cryptoStatus = CCCrypt(Option,Algorithm,0,Key.bytes,KeyLength,nil, Data.bytes, Data.count, cryptData1.mutableBytes, cryptData1.length,&numBytesEncrypted)
-    print(cryptoStatus)
+    let cryptoStatus = CCCrypt(Option,Algorithm,0,Key.bytes,KeyLength,iv.bytes, Data.bytes, Data.count, cryptData1.mutableBytes, cryptData1.length,&numBytesEncrypted)
     if UInt32(cryptoStatus) == UInt32(kCCSuccess) {
         //Convert NSMutualData to NSData to Data
         let data = NSData(data: cryptData1 as Data) as Data
@@ -124,19 +205,17 @@ func DESEnc(Input:String,Key:String,Iv:String = "0000000000000000")->String{
     }
 }
 
-func DESDec(Input:String,Key:String,Iv:String = "0000000000000000")->String{
+func DESDecCBC(Input:String,key:String,Iv:String = "0000000000000000")->String{
     //MARK: Round 1 - Encryption
     let Option = UInt32(kCCDecrypt)
     let Algorithm = UInt32(kCCAlgorithmDES)
-    //let Key = firstKey.hexadecimal! as NSData
-    let Key = Key.hexadecimal! as NSData
+    let Key = key.hexadecimal! as NSData
     let KeyLength = size_t(kCCKeySizeDES)
     let Data = Input.hexadecimal! as NSData
     let iv = Iv.hexadecimal! as NSData
     let cryptData1 = NSMutableData(length: Int(Data.length))!
     var numBytesEncrypted :size_t = 0
-    let cryptoStatus = CCCrypt(Option,Algorithm,0,Key.bytes,KeyLength,nil, Data.bytes, Data.count, cryptData1.mutableBytes, cryptData1.length,&numBytesEncrypted)
-    print(cryptoStatus)
+    let cryptoStatus = CCCrypt(Option,Algorithm,0,Key.bytes,KeyLength,iv.bytes, Data.bytes, Data.count, cryptData1.mutableBytes, cryptData1.length,&numBytesEncrypted)
     if UInt32(cryptoStatus) == UInt32(kCCSuccess) {
         //Convert NSMutualData to NSData to Data
         let data = NSData(data: cryptData1 as Data) as Data
@@ -145,13 +224,79 @@ func DESDec(Input:String,Key:String,Iv:String = "0000000000000000")->String{
     }else{
         return "Fail"
     }
+}
+
+func DESEncECB(Input:String,key:String)->String{
+    let Option = UInt32(kCCEncrypt)
+    let Algorithm = UInt32(kCCAlgorithmDES)
+    let Key = key.hexadecimal! as NSData
+    let KeyLength = size_t(kCCKeySizeDES)
+    let Data = Input.hexadecimal! as NSData
+    let cryptData1 = NSMutableData(length: Int(Data.length))!
+    var numBytesEncrypted :size_t = 0
+    let cryptoStatus = CCCrypt(Option,Algorithm,UInt32(kCCOptionECBMode),Key.bytes,KeyLength,nil, Data.bytes, Data.count, cryptData1.mutableBytes, cryptData1.length,&numBytesEncrypted)
+    if UInt32(cryptoStatus) == UInt32(kCCSuccess) {
+        //Convert NSMutualData to NSData to Data
+        let data = NSData(data: cryptData1 as Data) as Data
+        return data.hexadecimal
+        
+    }else{
+        return "Fail"
+    }
+}
+
+func DESDecECB(Input:String,key:String)->String{
+    let Option = UInt32(kCCDecrypt)
+    let Algorithm = UInt32(kCCAlgorithmDES)
+    let Key = key.hexadecimal! as NSData
+    let KeyLength = size_t(kCCKeySizeDES)
+    let Data = Input.hexadecimal! as NSData
+    let cryptData1 = NSMutableData(length: Int(Data.length))!
+    var numBytesEncrypted :size_t = 0
+    let cryptoStatus = CCCrypt(Option,Algorithm,UInt32(kCCOptionECBMode),Key.bytes,KeyLength,nil, Data.bytes, Data.count, cryptData1.mutableBytes, cryptData1.length,&numBytesEncrypted)
+    if UInt32(cryptoStatus) == UInt32(kCCSuccess) {
+        //Convert NSMutualData to NSData to Data
+        let data = NSData(data: cryptData1 as Data) as Data
+        return data.hexadecimal
+        
+    }else{
+        return "Fail"
+    }
+}
+
+// Adjust parity bit for key
+func AdjustParity(key:String)->String {
+    let binArr = DivideInputToEach8BitBlock(HexInput: key.hexaToBinary)
+    var result:[String] = []
+    var result2:[String] = []
+    for data in binArr {
+        var count = 0
+        var binn:[Character] = []
+        for bit in data {
+            binn.append(bit)
+            if bit == "1" {
+                count += 1
+            }
+        }
+        if count % 2 == 0{
+            if binn[binn.count-1] == "1" {
+                binn[binn.count-1] = "0"
+            }else{
+                binn[binn.count-1] = "1"
+            }
+        }
+        result.append(String(binn))
+    }
+    for bin in result {
+        result2.append(binToHex(bin)!)
+    }
+    return result2.joined()
 }
 
 
 
 
 // Hex String to Data
-
 extension String {
     
     /// Create `Data` from hexadecimal string representation
@@ -192,7 +337,6 @@ extension String {
 
 
 // Data to Hex String
-
 extension Data {
     
     /// Hexadecimal string representation of `Data` object.
@@ -212,6 +356,38 @@ extension Data {
     public func checkSum() -> Int {
         return self.map { Int($0) }.reduce(0, +) & 0xff
     }
+}
+
+// Hex to binary
+extension String {
+    typealias Byte = UInt8
+    var hexaToBytes: [Byte] {
+        var start = startIndex
+        return stride(from: 0, to: count, by: 2).compactMap { _ in   // use flatMap for older Swift versions
+            let end = index(after: start)
+            defer { start = index(after: end) }
+            return Byte(self[start...end], radix: 16)
+        }
+    }
+    var hexaToBinary: String {
+        return hexaToBytes.map {
+            let binary = String($0, radix: 2)
+            return repeatElement("0", count: 8-binary.count) + binary
+        }.joined()
+    }
+}
+
+// Binary to hex
+func binToHex(_ bin : String) -> String? {
+    // binary to integer:
+    guard let num = UInt64(bin, radix: 2) else { return nil }
+    // integer to hex:
+    let hex = String(num, radix: 16,uppercase: true) // (or false)
+    if hex.count < 2 {
+        let h = "0" + hex
+        return h
+    }
+    return hex
 }
 
 
